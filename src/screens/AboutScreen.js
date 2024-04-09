08ee0efcc90d5c873d4d5def26274858a935a20b
Andrew-Reimert <andrew.reimert@edu.sait.ca>
2024-04-09 16:07:30 -0600

    More Styling

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -7,16 +7,21 @@ const assetDir = "../../assets/img/";
 export default function AboutScreen({goToScreen}){
     return (
         <SafeAreaView style={styles.body}>
-            <Text>Starfish VS Pufferfish Tic-Tac-Toe</Text>
+            <View style={styles.infocontainer}>
+            <Text style={styles.title}>The Tale of War</Text>
+            <Text>In the endless depths of the ocean, a bitter feud ensues between the starfish and the pufferfish. What began as a clash over territory has evolved into a relentless war, shaping the undersea landscape for generations. Despite the vastness of the ocean, both sides are locked in a ceaseless struggle for dominance. Whispers of a prophecy speak of a chosen one who will turn the tide leading one side to victory, but which one?</Text>
+            <Text>Starfish VS Pufferfish Age of War</Text>
             <Text>By:</Text>
             <Text>Brandt Fowler - Lead Programmer, Lead Artist</Text>
             <Text>Andrew Reimert</Text>
             <Text>Adam McDougal</Text>
             <Text>Rhianna Smith</Text>
+            
             {/* Back To Menu Button */}
             <Pressable style = {styles.backButton} onPress={() => goToScreen("Home")}>
                 <Image source={require(assetDir + "back.png")} style={styles.cancelIcon} />
             </Pressable>
+            </View>
         </SafeAreaView>
     )
 }
@@ -24,6 +29,7 @@ export default function AboutScreen({goToScreen}){
 const styles = StyleSheet.create({
     body:{
         flex: 1,
+        alignItems: "center",
     },
     backButton:{
         position: 'absolute',
@@ -31,7 +37,24 @@ const styles = StyleSheet.create({
         left:0,
     },
     cancelIcon:{
-      width: 100,
-      height: 100
+      width: 50,
+      height: 50,
+      margin: 10,
+    },
+    infocontainer:{
+        flex: 1,
+        width: 350,
+        backgroundColor: "#c9a053",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 10,
+        margin: 10,
+        borderColor: "#644D3E",
+        borderWidth: 3,
+    },
+    title:{
+        fontSize: 35,
+        paddingTop: 35,
+        fontWeight: "bold",
     },
 });
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -9,23 +9,27 @@ export default function HomeScreen({goToScreen, onChangeBG}){
         <SafeAreaView style={styles.body}>
             
             {/* Title */}
-            <Text style={styles.title}> Starfish Vs Pufferfish</Text>
-            <Text>Age of War</Text>
+            <Text style={styles.title}> Starfish vs Pufferfish</Text>
+            <Text style={styles.subtitle}>Age of War</Text>
         
             {/* Menu Buttons */}
-            <Pressable onPress={() => goToScreen("GameCPU")}>
-                <Text>Play V.S. Computer</Text>
+            <View style={styles.buttonContainer}>
+            <Pressable style={styles.button} onPress={() => goToScreen("GameCPU")}>
+                <Text style={styles.buttonTxt}>Play V.S. Computer</Text>
             </Pressable>
 
-            <Pressable onPress={() => goToScreen("Game")}>
-                <Text>Play V.S. Player</Text>
+            <Pressable style={styles.button} onPress={() => goToScreen("Game")}>
+                <Text style={styles.buttonTxt}>Play V.S. Player</Text>
             </Pressable>
 
-            <Pressable onPress={() => goToScreen("About")}>
-                <Text>About</Text>
+            <Pressable style={styles.button} onPress={() => goToScreen("About")}>
+                <Text style={styles.buttonTxt}>About</Text>
             </Pressable>
+            </View>
 
-            <Pressable onPress={() => onChangeBG(1)}>
+            {/* Background Buttons */}
+
+            <Pressable  onPress={() => onChangeBG(1)}>
                 <Text>Background 1</Text>
             </Pressable>
 
@@ -49,8 +53,34 @@ const styles = StyleSheet.create({
         position:"absolute",
     },
     title:{
-        fontSize: 40,
-        fontFamily: "",
+        fontSize: 35,
+        paddingTop: 35,
+        fontWeight: "bold",
         textAlign: "center",
     },
+    subtitle:{
+        fontSize: 20,
+        textAlign: "center",
+        fontWeight: "bold",
+    },
+    buttonContainer:{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    button:{
+        width: 200,
+        height: 50,
+        backgroundColor: "#a87f32",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 10,
+        margin: 10,
+        borderColor: "#644D3E",
+        borderWidth: 2,
+    },
+    buttonTxt:{
+        fontSize: 20,
+        color: "white",
+    }
 });
