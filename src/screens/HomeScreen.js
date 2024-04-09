5416298cf26a543f5c6f394e4ee57b58eb53ea5a
BrandtFgit <Brandt.fowler@edu.sait.ca>
2024-04-09 12:44:57 -0600

    Update HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -7,18 +7,21 @@ const assetDir = "../../assets/img/";
 export default function HomeScreen({goToScreen}){
     return (
         <SafeAreaView style={styles.body}>
+            {/* Title */}
             <Text> Starfish Vs Pufferfish Tic-Tac-Toe </Text>
         
-        {/* Menu Buttons */}
-        <Pressable onPress={() => goToScreen("GameCPU")}>
-            <Text>Play V.S. Computer</Text>
-        </Pressable>
-        <Pressable onPress={() => goToScreen("Game")}>
-            <Text>Play V.S. Player</Text>
-        </Pressable>
-        <Pressable onPress={() => goToScreen("Game")}>
-            <Text>About</Text>
-        </Pressable>
+            {/* Menu Buttons */}
+            <Pressable onPress={() => goToScreen("GameCPU")}>
+                <Text>Play V.S. Computer</Text>
+            </Pressable>
+
+            <Pressable onPress={() => goToScreen("Game")}>
+                <Text>Play V.S. Player</Text>
+            </Pressable>
+            
+            <Pressable onPress={() => goToScreen("Game")}>
+                <Text>About</Text>
+            </Pressable>
         </SafeAreaView>
     )
 }
